c2d5528f0cc4878de66b8486006b823f371c9b74
nu.everest <nu.everest@gmail.com>
2017-03-19 14:47:16 -0700

    Added scorePassword function.

---
+++ src/components/Signup.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="small-12 medium-7 large-5 small-centered columns bgc-h76b852 border-radius-3 margin-top-35">
+    <div class="text-align-center font-size-32-s white padding-top-20">Sign Up or Login <i class="material-icons font-size-36-i">&#xE85E</i></div>
+
+    <div class="row">
+      <div class="small-11 columns small-centered margin-top-20">
+        <div class="width-93p bgc-white font-size-1 line-height-9 margin-0-auto">&nbsp</div>
+        <form id="userform" class="form input-underline" v-on:submit.prevent="addUser">
+          <div class="row">
+            <div class="small-12 columns padding-top-20">
+              Email
+              <span class="hb85276 float-right" v-show="!validation.email">Required</span>
+              <span class="hb85276 float-right" v-show="validation.email && !validation.emaillength">Max Len {{ maxLength }}</span>
+              <span class="hb85276 float-right" v-show="validation.email && validation.emaillength && !validation.emailformat">Example: me@a.com</span>
+              <input id="email" type="text" v-model="newUser.email" placeholder="me@a.com" v-bind:maxlength="maxLength">
+            </div>
+          </div>
+
+          <div class="row margin-top-15">
+            <div class="small-12 columns">
+              Password <span class="hb85276 float-right" v-show="!validation.password">Required</span>
+              <span class="hb85276 float-right" v-show="validation.password && !validation.passwordlength">Max Len {{ maxLength }}</span>
+              <input id="password" type="password" v-model="newUser.password" v-bind:maxlength="maxLength">
+            </div>
+          </div>
+
+          <div class="row margin-top-15">
+            <div class="small-12 columns">
+              Confirm Password <span class="hb85276 float-right" v-show="!validation.confirmpassword">Required</span>
+              <input id="confirmPassword" type="password" v-model="newUser.confirmPassword" v-bind:maxlength="maxLength">
+            </div>
+          </div>
+
+          <div class="row text-align-center">
+            <div class="small-12 columns">
+              <span class="hb85276" v-show="!validation.confirmpassword || validation.passwordsmatch">&nbsp</span>
+              <span class="hb85276" v-show="validation.confirmpassword && !validation.passwordsmatch">Passwords Must Match</span>
+            </div>
+          </div>
+
+          <div class="row margin-top-20 text-align-center">
+            <div class="small-12 columns">
+              <button id="submit" class="button font-size-20-s padding-16-s-i border-radius-5" v-bind:disabled="!isValid" type="submit" name="submit">
+                <span>Sign Up</span>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signup',
+  data () {
+    return {
+      emailRE: /\S+@\S+/,
+      maxLength: 254,       // Email Maximum Length Reference: https://en.wikipedia.org/wiki/Email_address
+      newUser: {
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }
+    }
+  },
+  method: {
+    addUser: function () {
+      if (this.isValid) {
+
+      }
+    }
+  },
+  computed: {
+    userExists: function () {
+      // TODO: If user exists sw
+      return true
+    },
+    validation: function () {
+      return {
+        email: !!this.newUser.email.trim(),
+        emaillength: this.newUser.email.trim().length <= this.maxLength,
+        emailformat: this.emailRE.test(this.newUser.email),
+        password: !!this.newUser.password.trim(),
+        passwordlength: this.newUser.password.trim().length <= this.maxLength,
+        confirmpassword: !!this.newUser.confirmPassword.trim(),
+        passwordsmatch: this.newUser.password.trim() === this.newUser.confirmPassword.trim()
+      }
+    },
+    isValid: function () {
+      let validation = this.validation
+      return Object.keys(validation).every(function (key) {
+        return validation[key]
+      })
+    },
+    scorePassword: function () {
+      /**
+       * Scores a password's strength.
+       *
+       * It scores a password according to character variation, repetition, username similarity, common patterns,
+       * and length.
+       *
+       * The passwords are scored on an integer scale with no upper or lower bound.
+       * Scores less than 0 are set to 0.
+       * Scores greater than 100 are set to 100.
+       * A safe password score is generally 49 points or more.
+       *
+       * @param {String} pwd The password string to score.
+       *
+       * @returns {Number} The password score.
+       *
+       * @see https://gist.github.com/stgogm/8a5edafa39f4d8d393520e2b40ee72e1
+       * @see https://stackoverflow.com/questions/948172/password-strength-meter/11268104#11268104
+      **/
+      let email = this.newUser.email
+      let password = this.newUser.password
+      let check, ltr, i, l
+      let variation = 0
+      let letters = {}
+      let lowerBound = 0
+      let upperBound = 100
+      let score = 0
+
+      // No Password
+      if (!password) {
+        return 0
+      }
+
+      // Password identical to email address.
+      if (password === email) {
+        return 0
+      }
+
+      /* Score character variation */
+      let variations = {
+        lower: /[a-z]/.test(password),
+        upper: /[A-Z]/.test(password),
+        nonWords: /\W/.test(password),
+        digits: /\d/.test(password)
+      }
+
+      for (check in variations) {
+        variation += variations[check] ? 1 : 0
+      }
+
+      score += (variation - 1) * 10
+
+      /* Score unique letters until 5 repetitions */
+      for (i = 0, l = password.length; i < l; i++) {
+        ltr = letters[password[i]] = (letters[password[i]] || 0) + 1
+        score += 5 / ltr
+      }
+
+      /* Score length (about 8 chars for a safe password) */
+      score -= 16 - (password.length / 16)
+
+      /* Set upper and lower bounds to 100 and 0 respectively. */
+      if (score > upperBound) { score = upperBound }
+      if (score < lowerBound) { score = lowerBound }
+
+      return parseInt(score)
+    }
+  }
+}
+</script>
--- src/components/SignupLogin.vue
+++ src/components/SignupLogin.vue
@@ -1,97 +1,22 @@
 <template>
-  <div class="small-12 medium-7 large-5 small-centered columns bgc-h76b852 border-radius-3 margin-top-35">
-    <div class="text-align-center font-size-32-s white padding-top-20">Sign Up or Login <i class="material-icons font-size-36-i">&#xE85E;</i></div>
-
-    <div class="row">
-      <div class="small-11 columns small-centered margin-top-20">
-        <div class="width-93p bgc-white font-size-1 line-height-9 margin-0-auto">&nbsp;</div>
-        <form id="userform" class="form input-underline" v-on:submit.prevent="addUser">
-          <div class="row">
-            <div class="small-12 columns padding-top-20">
-              Email
-              <span class="hb85276 float-right" v-show="!validation.email">Required</span>
-              <span class="hb85276 float-right" v-show="validation.email && !validation.emaillength">Max Len {{ maxLength }}</span>
-              <span class="hb85276 float-right" v-show="validation.email && validation.emaillength && !validation.emailformat">Example: me@a.com</span>
-              <input id="email" type="text" v-model="newUser.email" placeholder="me@a.com" v-bind:maxlength="maxLength">
-            </div>
-          </div>
-
-          <div class="row margin-top-15">
-            <div class="small-12 columns">
-              Password <span class="hb85276 float-right" v-show="!validation.password">Required</span>
-              <span class="hb85276 float-right" v-show="validation.password && !validation.passwordlength">Max Len {{ maxLength }}</span>
-              <input id="password" type="password" v-model="newUser.password" v-bind:maxlength="maxLength">
-            </div>
-          </div>
-
-          <div class="row margin-top-15">
-            <div class="small-12 columns">
-              Confirm Password <span class="hb85276 float-right" v-show="!validation.confirmpassword">Required</span>
-              <input id="confirmPassword" type="password" v-model="newUser.confirmPassword" v-bind:maxlength="maxLength">
-            </div>
-          </div>
-
-          <div class="row text-align-center">
-            <div class="small-12 columns">
-              <span class="hb85276" v-show="!validation.confirmpassword || validation.passwordsmatch">&nbsp;</span>
-              <span class="hb85276" v-show="validation.confirmpassword && !validation.passwordsmatch">Passwords Must Match</span>
-            </div>
-          </div>
-
-          <div class="row margin-top-20 text-align-center">
-            <div class="small-12 columns">
-              <button id="submit" class="button font-size-20-s padding-16-s-i border-radius-5" v-bind:disabled="!isValid" type="submit" name="submit">
-                <span>Sign Up</span>
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
+  <component v-bind:is="currentView"></component>
 </template>
 
 <script>
+import Signup from '@/components/Signup'
+
 export default {
   name: 'SignupLogin',
-  data () {
-    return {
-      emailRE: /\S+@\S+/,
-      maxLength: 254,       // Email Maximum Length Reference: https://en.wikipedia.org/wiki/Email_address
-      newUser: {
-        email: '',
-        password: '',
-        confirmPassword: ''
-      }
-    }
-  },
-  method: {
-    addUser: function () {
-      if (this.isValid) {
-
-      }
-    }
+  components: {
+    login: {},
+    signup: Signup,
+    forgotpassword: {},
+    changepassword: {},
+    confirm: {}
   },
   computed: {
-    userExists: function () {
-      return true
-    },
-    validation: function () {
-      return {
-        email: !!this.newUser.email.trim(),
-        emaillength: this.newUser.email.trim().length <= this.maxLength,
-        emailformat: this.emailRE.test(this.newUser.email),
-        password: !!this.newUser.password.trim(),
-        passwordlength: this.newUser.password.trim().length <= this.maxLength,
-        confirmpassword: !!this.newUser.confirmPassword.trim(),
-        passwordsmatch: this.newUser.password.trim() === this.newUser.confirmPassword.trim()
-      }
-    },
-    isValid: function () {
-      let validation = this.validation
-      return Object.keys(validation).every(function (key) {
-        return validation[key]
-      })
+    currentView: function () {
+      return 'signup'
     }
   }
 }
