aeb8b4a16219c1ce2bcc139895499ab514cfae03
nu.everest <nu.everest@gmail.com>
2017-03-26 15:03:40 -0700

    Added a unit test for social component.

---
+++ test/unit/specs/Social.spec.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Social from '@/components/Social'
+
+// ExpectJS reference: https://github.com/Automattic/expect.js
+
+describe('Social.vue', () => {
+  it(`should render the -OR- separator`, () => {
+    // Extend the component to get the constructor, which we can then initialize directly.
+    const Constructor = Vue.extend(Social)
+    const component = new Constructor().$mount()
+    expect(component.$el.querySelector('.font-size-13').textContent).to.contain('——————  OR  ——————')
+  })
+})
